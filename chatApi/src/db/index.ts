39d4876148233cd37b27b7da04773cbd800b5184
bishri <bishri60@gmail.com>
2025-02-27 15:16:51 +0300

    add initial database configuration and setup script

---
+++ chatApi/src/db/index.ts
@@ -0,0 +1,11 @@
+// import { User } from "../models/User";
+// import { sequelize } from "./dbconfig";
+
+// sequelize
+//   .sync()
+//   .then(() => {
+//     console.log("Database & tables created!");
+//   })
+//   .catch((error) => {
+//     console.error("Error creating database & tables:", error);
+//   });
